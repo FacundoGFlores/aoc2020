3a5e2776b1d3b6302942e43c0d6bf2cb87ac2cd0
Facundo Flores <flores.facundogabriel@gmail.com>
2020-12-06 22:02:50 -0300

    Day 2 part 2

--- day2/part2.rb
+++ day2/part2.rb
@@ -9,8 +9,8 @@ def parse_data(list)
   list.map do |item|
     policy_builder = item.match(/(^\d*)-(\d*) (\w): (\w*)/)
     {
-      min: policy_builder[1].to_i,
-      max: policy_builder[2].to_i,
+      valid_pos: policy_builder[1].to_i - 1,
+      invalid_pos: policy_builder[2].to_i - 1,
       symbol: policy_builder[3],
       password: policy_builder[4]
     }
@@ -18,19 +18,18 @@ def parse_data(list)
 end
 
 def policy_checker(policy)
-  counted_symbols = policy[:password].count(policy[:symbol])
-  counted_symbols >= policy[:min] && counted_symbols <= policy[:max]
+  (policy[:password][policy[:valid_pos]] == policy[:symbol] && policy[:password][policy[:invalid_pos]] != policy[:symbol]) || (policy[:password][policy[:valid_pos]] != policy[:symbol] && policy[:password][policy[:invalid_pos]] == policy[:symbol])
 end
 
 def filter_passwords(list)
   list.select { |policy| policy_checker(policy) }
 end
 
-def part1
+def part2
   data = retrieve_data
   parsed_data = parse_data(data)
   result = filter_passwords(parsed_data)
   puts result.size
 end
 
-part1
+part2
